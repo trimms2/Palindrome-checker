5d37a2f87c7c42f700a55ce8853d65fa42165a7e
hedoescode-mayank <md6074@srmist.edu.in>
2026-03-03 09:25:47 +0530

    UC3-Using string

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        String word = "madam";
 
+
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
     }
 }
