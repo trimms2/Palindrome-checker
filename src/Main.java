2999615e0565a77a8b77c843da52a2b9aa483829
hedoescode-mayank <md6074@srmist.edu.in>
2026-03-03 09:19:51 +0530

    UC2-Reversing method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+
+
+        String input="madam";
+        boolean isPalindrome=true;
+        String reversed="";
+        int n=input.length();
+        for(int i =n;i>=0;i--) {
+            reversed=reversed+input.charAt(i);
+        }
+
+        if(input.equals(reversed)) {
+
+            System.out.println("Is it Palindrome "+isPalindrome);
+        }
+
     }
+
 }
