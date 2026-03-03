5a199279fdb559daf1bf5cae539932882ce368dc
hedoescode-mayank <md6074@srmist.edu.in>
2026-03-03 09:13:01 +0530

    UC1- Basic Palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+
+        String input = "madams";
+
+        boolean isPalindrome = true;
+
+
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input Text: " + input);
+        System.out.println("Is it Palindrome: " + isPalindrome);
     }
 }
