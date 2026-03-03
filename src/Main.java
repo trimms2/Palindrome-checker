5f902846021a65bcfe44b539a1d47403c2887d46
hedoescode-mayank <md6074@srmist.edu.in>
2026-03-03 09:42:36 +0530

    UC10 Ignore spaces and case while checking a palindrome.

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,31 @@
 import java.util.*;
-public class Main {
-    static class Node {
-        char data;
-        Node next;
+public static void main(String[] args) {
 
-        Node(char data) {
-            this.data = data;
-            this.next = null;
+    String original = "A man a plan a canal Panama";
+
+    String normalized = original.replaceAll("\\s+", "").toLowerCase();
+
+    int start = 0;
+    int end = normalized.length() - 1;
+
+    boolean isPalindrome = true;
+
+    while (start < end) {
+        if (normalized.charAt(start) != normalized.charAt(end)) {
+            isPalindrome = false;
+            break;
         }
+        start++;
+        end--;
     }
 
-    public static void main(String[] args) {
-
-        String original = "madam";
-
-        Node head = null;
-        Node tail = null;
-
-        for (int i = 0; i < original.length(); i++) {
-            Node newNode = new Node(original.charAt(i));
-
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-
-        if (isPalindrome(head)) {
-            System.out.println(original + " is a Palindrome.");
-        } else {
-            System.out.println(original + " is NOT a Palindrome.");
-        }
+    if (isPalindrome) {
+        System.out.println(original + " is a Palindrome.");
+    } else {
+        System.out.println(original + " is NOT a Palindrome.");
     }
+}
 
-    static boolean isPalindrome(Node head) {
-
-        if (head == null || head.next == null)
-            return true;
-
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node prev = null;
-        Node current = slow;
-
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-
-        Node firstHalf = head;
-        Node secondHalf = prev;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data)
-                return false;
-
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
-    }
 
 
 
